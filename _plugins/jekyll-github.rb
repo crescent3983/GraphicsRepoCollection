c9237bf303459e6eff15a1d9ab1adff37f33b93d
David Kuo <d124154153@gmail.com>
2022-04-17 00:03:42 +0800

    add new content

--- _plugins/jekyll-github.rb
+++ _plugins/jekyll-github.rb
@@ -6,9 +6,14 @@ module Jekyll
         def generate(site)
             project_list = {}
             site.data['projects'].each { |project|
-                info = JSON.load(URI.open('https://api.github.com/repos/' + project['author'] + '/' + project['name']))
-                project['stargazers_count'] = info['stargazers_count']
-                project['pushed_at'] = info['pushed_at']
+                begin
+                    info = JSON.load(URI.open('https://api.github.com/repos/' + project['author'] + '/' + project['name']))
+                    project['stargazers_count'] = info['stargazers_count']
+                    project['pushed_at'] = info['pushed_at']
+                rescue
+                    project['stargazers_count'] = 0
+                    project['pushed_at'] = '1970/01/01'
+                end
 
                 project['topics'].each { |topic|
                     unless project_list.key?(topic)

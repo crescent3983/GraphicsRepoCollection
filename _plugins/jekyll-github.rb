ca852cdeb031af1b028609a0f81661f7e6ac2d9e
David Kuo <d124154153@gmail.com>
2022-04-20 23:22:15 +0800

    split project to multiple files

--- _plugins/jekyll-github.rb
+++ _plugins/jekyll-github.rb
@@ -1,31 +1,34 @@
 require 'open-uri'
 require 'json'
+require 'pp'
 
 module Jekyll
     class ProjectGenerator < Jekyll::Generator
         def generate(site)
             project_list = {}
-            site.data['projects'].each { |project|
-                begin
-                    url = 'https://api.github.com/repos/' + project['author'] + '/' + project['name']
-                    info = {}
-                    if site.config.key?("github_token")
-                        info = JSON.load(URI.open(url, "Authorization" => "Bearer #{site.config['github_token']}"))
-                    else
-                        info = JSON.load(URI.open(url))
+            site.data['projects'].each { |topic|
+                topic[1].each { |project|
+                    begin
+                        url = 'https://api.github.com/repos/' + project['author'] + '/' + project['name']
+                        info = {}
+                        if site.config.key?("github_token")
+                            info = JSON.load(URI.open(url, "Authorization" => "Bearer #{site.config['github_token']}"))
+                        else
+                            info = JSON.load(URI.open(url))
+                        end
+                        project['stargazers_count'] = info['stargazers_count']
+                        project['pushed_at'] = info['pushed_at']
+                    rescue
+                        project['stargazers_count'] = 0
+                        project['pushed_at'] = '1970/01/01'
                     end
-                    project['stargazers_count'] = info['stargazers_count']
-                    project['pushed_at'] = info['pushed_at']
-                rescue
-                    project['stargazers_count'] = 0
-                    project['pushed_at'] = '1970/01/01'
-                end
 
-                project['topics'].each { |topic|
-                    unless project_list.key?(topic)
-                        project_list[topic] = []
-                    end
-                    project_list[topic].append(project)
+                    project['topics'].each { |topic|
+                        unless project_list.key?(topic)
+                            project_list[topic] = []
+                        end
+                        project_list[topic].append(project)
+                    }
                 }
             }
             site.data['project_list'] = project_list
